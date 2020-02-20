71c9c3aacc8a01c66ca8a645d44fadcaf6cdd424
ObsidianDestroyer <a.vasiliev@ongrid.pro>
2020-02-20 18:01:22 +0300

    Improved incoming request handling

--- src/routes/web_proxy.py
+++ src/routes/web_proxy.py
@@ -30,30 +30,44 @@ async def request_ton_site(request: web.Request) -> web.Response:
     )
     request_body = await request.json()
     path = request_body['path']
+    if len(path) != 0:
+        if 'http' or 'https' not in path:
+            path = f'http://{path}'
+            return await profixy_request(path)
 
-    if 'http' or 'https' not in path:
-        path = f'http://{path}'
-        return await profixy_request(path)
+        elif 'http' or 'https' in path:
+            return await profixy_request(path)
 
-    elif 'http' or 'https' in path:
-        return await profixy_request(path)
-
+        else:
+            return web.Response(
+                text=(
+                    'Wrong request! ' f'Try to to use with proto: http://{path}'
+                ),
+                status=404,
+            )
     else:
         return web.Response(
             text=(
-                'Wrong request! ' f'Try to to use with proto: http://{path}'
+                'You are requested an empty address!'
             ),
-            status=404,
+            status=404
         )
 
 
 async def profixy_request(path):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(path, proxy=target_url, timeout=30) as response:
-            raw_response = await response.text(encoding='utf-8')
-            print(f'Proxifying request of "{path}"')
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(path, proxy=target_url, timeout=30) as response:
+                raw_response = await response.text(encoding='utf-8')
+                print(f'Proxifying request of "{path}"')
+            return web.Response(
+                body=raw_response,
+                status=response.status,
+                headers={'Content-Type': 'text/html', 'Referer': target_url},
+            )
+    except Exception as err:
         return web.Response(
-            body=raw_response,
-            status=response.status,
-            headers={'Content-Type': 'text/html', 'Referer': target_url},
+            text=f'Occurred error while requesting specified address: "{path}"'
+                 f'Error context: {err.__context__}',
+            status=400
         )
