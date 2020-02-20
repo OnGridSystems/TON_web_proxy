03468004f2ecf79b8b19484a410ed557c2293857
ObsidianDestroyer <a.vasiliev@ongrid.pro>
2020-02-20 14:38:16 +0300

    Small rework for request proxifying

--- src/routes/web_proxy.py
+++ src/routes/web_proxy.py
@@ -1,11 +1,12 @@
 import aiohttp
 import aiohttp_jinja2
-
 from aiohttp import web
 
 from src.settings import PROXY_SERVER_ADDRESS, PROXY_SERVER_PORT
 
 route_web_proxy = web.RouteTableDef()
+
+target_url = f'http://{PROXY_SERVER_ADDRESS}:{PROXY_SERVER_PORT}'
 
 
 @route_web_proxy.get('/')
@@ -16,9 +17,7 @@ async def index_ton_proxy(request: web.Request) -> web.Response:
     )
     context = {}
     response = aiohttp_jinja2.render_template(
-        'templates/base_layout.html',
-        request,
-        context
+        'templates/base_layout.html', request, context
     )
     return response
 
@@ -29,42 +28,32 @@ async def request_ton_site(request: web.Request) -> web.Response:
         f'Incoming request < [{request.method}] > '
         f'from: {request.host}{request.path}'
     )
-    target_url = (
-        f'http://{PROXY_SERVER_ADDRESS}:{PROXY_SERVER_PORT}'
-    )
     request_body = await request.json()
     path = request_body['path']
-    print(f'Target URL: {target_url}')
-    print(f'Target PATH: {path}')
+
     if 'http' or 'https' not in path:
         path = f'http://{path}'
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                    path, proxy=target_url) as response:
-                raw_response = await response.text(encoding='utf-8')
-            return web.Response(
-                body=raw_response,
-                status=response.status,
-                headers={
-                    'Content-Type': 'text/html',
-                    'Referer': target_url
-                }
-            )
+        return await profixy_request(path)
+
     elif 'http' or 'https' in path:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                    path, proxy=target_url, timeout=30) as response:
-                raw_response = await response.text(encoding='utf-8')
-            return web.Response(
-                body=raw_response,
-                status=response.status,
-                headers={
-                    'Content-Type': 'text/html',
-                    'Referer': target_url
-                }
-            )
+        return await profixy_request(path)
+
     else:
         return web.Response(
-            text='You have been sent some shit to me, damn!',
-            status=404
+            text=(
+                'Wrong request! ' f'Try to to use with proto: http://{path}'
+            ),
+            status=404,
+        )
+
+
+async def profixy_request(path):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(path, proxy=target_url, timeout=30) as response:
+            raw_response = await response.text(encoding='utf-8')
+            print(f'Proxifying request of "{path}"')
+        return web.Response(
+            body=raw_response,
+            status=response.status,
+            headers={'Content-Type': 'text/html', 'Referer': target_url},
         )
